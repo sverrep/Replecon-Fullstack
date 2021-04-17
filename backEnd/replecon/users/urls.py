2e209b913039c276f3042f0097a878ff023be2d3
steveno06 <61500424+steveno06@users.noreply.github.com>
2021-04-17 16:10:23 +0200

    Balance API Added to profile screen

--- backEnd/replecon/users/urls.py
+++ backEnd/replecon/users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from django.conf.urls import url
 from rest_framework.authtoken.views import obtain_auth_token
-from .views import CreateUserAPIView, LogoutUserAPIView, UserViewSet, StudentDetails
+from .views import CreateUserAPIView, LogoutUserAPIView, UserViewSet, StudentClassCode, StudentBalance
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
@@ -12,5 +12,6 @@ urlpatterns = [
     url('auth/register/$', CreateUserAPIView.as_view(), name = 'auth_user_create'),
     url('auth/logout/$', LogoutUserAPIView.as_view(), name = 'auth_user_logout'),
     path('', include(router.urls)),
-    url('students/class_code/', StudentDetails.as_view()),
+    url('students/class_code/', StudentClassCode.as_view()),
+    url('students/balance/', StudentBalance.as_view()),
 ]
