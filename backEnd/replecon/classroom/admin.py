629d38109f4c949850c8269169920f97c76f9b39
sverrep <sverrep@users.noreply.github.com>
2021-04-18 18:09:40 +0200

    Added back end functionality for retrieving all students with the same class code

--- backEnd/replecon/classroom/admin.py
+++ backEnd/replecon/classroom/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Classroom
 
 # Register your models here.
+
+@admin.register(Classroom)
+class ClassroomModel(admin.ModelAdmin):
+    list_filter = ('class_name', 'teacher_id')
+    list_display = ('class_name', 'teacher_id')
