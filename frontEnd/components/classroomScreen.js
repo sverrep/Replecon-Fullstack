08109354b33cfc504177fdb3e14b61745d1ed5ca
steveno06 <61500424+steveno06@users.noreply.github.com>
2021-04-19 15:59:33 +0200

    Added Pop-Up

--- frontEnd/components/classroomScreen.js
+++ frontEnd/components/classroomScreen.js
@@ -4,20 +4,21 @@ import {
   Text,
   View,
   FlatList,
+  Modal,
   
 } from "react-native";
 
 import { useNavigation } from '@react-navigation/native';
 import styles from '../componentStyles.js'
 import { NavigationContainer } from '@react-navigation/native';
-import { Button, Card, Avatar, Title, Paragraph } from 'react-native-paper';
+import { Button, Card, TextInput, IconButton } from 'react-native-paper';
 
 const mydata = [
   {id: 1, name: 'steven'},
-  {id: 2,  name: 'steven'},
-  {id: 3,  name: 'steven'},
-  {id: 4,  name: 'steven'},
-  {id: 5,  name: 'steven'},
+  {id: 2,  name: 'john'},
+  {id: 3,  name: 'sverre'},
+  {id: 4,  name: 'bob'},
+  {id: 5,  name: 'lolxdgottem'},
   {id: 6,  name: 'steven'},
   {id: 7,  name: 'steven'},
   {id: 8, name: 'steven'},
@@ -27,18 +28,36 @@ const mydata = [
   {id: 12,  name: 'steven'},
 ]
 
-const renderData = (item) => {
-  return(
-    
-    <Card style={styles.studentCards}>
-      <Text style={{ textAlign: "left" }}> {item.name}</Text>
-    </Card>
-  )
 
-}
+
+
 
 
 class ClassroomScreen extends Component {
+  constructor(){
+    super();
+    this.state={
+      show:false,
+      name:"",
+    }
+
+  }
+
+  clickedItem = (data) => {
+    this.setState({show:true})
+    this.setState({name:data.name})
+  }
+
+  renderData = (item) => {
+    return(
+      
+      <Card style={styles.studentCards} onPress = {() => this.clickedItem(item)}>
+        <Text style={{ textAlign: "left" }}> {item.name}</Text>
+      </Card>
+    )
+  
+  }
+
   render() {
     return (
       <View style={[styles.classroomContainer, {
@@ -47,19 +66,47 @@ class ClassroomScreen extends Component {
       
         <View style={{ flex: 1 }}>
           <Text style={styles.header}>This is the classroom Screen</Text>
+          
         </View>
 
         <View style={{ flex: 4}}>
-          
+
             <View>
               <FlatList
                 data = {mydata}
                 renderItem = {({item})=> {
-                  return renderData(item)
+                  return this.renderData(item)
                 }}
                 keyExtractor = {item => item.id.toString()}
               />
             </View>
+            <Modal
+            transparent = {true}
+            visible = {this.state.show}
+            >
+              <View style = {{backgroundColor:'#000000aa', flex:1}}>
+                <View style = {{backgroundColor:'#ffffff', margin:50, padding:40, borderRadius:10, marginTop:200, bottom: 50, flex:1}} >
+                  <View style = {{position: 'absolute', right:0, top:0}}>
+                    <IconButton
+                      icon="close-box-outline"
+                      color= 'grey'
+                      size={20}
+                      onPress={() => {this.setState({show:false})}}
+                    />
+                  </View>
+                  <Text>Recipient: {this.state.name}</Text>
+                  <TextInput
+                    label="Amount"
+                    mode = 'outlined'
+
+                  ></TextInput>
+                  <Button onPress={() => {this.setState({show:false})}}>Send Money</Button>
+                </View>
+
+              </View>
+
+
+            </Modal>
           
         </View>
       </View>
