68e09746142bb38c56ea95a24df14f1cec402224
steveno06 <61500424+steveno06@users.noreply.github.com>
2021-04-19 14:13:06 +0200

    Listing Functions/Fixed Imports

--- frontEnd/components/classroomScreen.js
+++ frontEnd/components/classroomScreen.js
@@ -1,24 +1,42 @@
 import 'react-native-gesture-handler';
 import React, { Component, useState } from "react";
 import {
-  StyleSheet,
   Text,
   View,
-  SafeAreaView,
-  Image,
-  TextInput,
-  TouchableOpacity,
-  StatusBar,
+  FlatList,
   
 } from "react-native";
 
 import { useNavigation } from '@react-navigation/native';
 import styles from '../componentStyles.js'
 import { NavigationContainer } from '@react-navigation/native';
-import { Button, Card, Avatar, Title, Paragraph  } from 'react-native-paper';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { Button, Card, Avatar, Title, Paragraph } from 'react-native-paper';
 
-const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
+const mydata = [
+  {id: 1, name: 'steven'},
+  {id: 2,  name: 'steven'},
+  {id: 3,  name: 'steven'},
+  {id: 4,  name: 'steven'},
+  {id: 5,  name: 'steven'},
+  {id: 6,  name: 'steven'},
+  {id: 7,  name: 'steven'},
+  {id: 8, name: 'steven'},
+  {id: 9,  name: 'steven'},
+  {id: 10,  name: 'steven'},
+  {id: 11,  name: 'steven'},
+  {id: 12,  name: 'steven'},
+]
+
+const renderData = (item) => {
+  return(
+    
+    <Card style={styles.studentCards}>
+      <Text style={{ textAlign: "left" }}> {item.name}</Text>
+    </Card>
+  )
+
+}
+
 
 class ClassroomScreen extends Component {
   render() {
@@ -33,22 +51,20 @@ class ClassroomScreen extends Component {
 
         <View style={{ flex: 4}}>
           
-            
             <View>
-              <Card style={styles.studentCards}>
-                <Text style={{ textAlign: "left" }}> Steven Ohrdorf</Text>
-              </Card>
+              <FlatList
+                data = {mydata}
+                renderItem = {({item})=> {
+                  return renderData(item)
+                }}
+                keyExtractor = {item => item.id.toString()}
+              />
             </View>
           
-          </View>
         </View>
-      
-      
-
-
+      </View>
     );
   }
-
 };
 
 export default function(props) {
