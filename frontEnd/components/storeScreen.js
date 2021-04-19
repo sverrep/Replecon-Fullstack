3f89b1ee2ebbbd6a3a64bd7e27fc470f57bb3f63
steveno06 <61500424+steveno06@users.noreply.github.com>
2021-04-19 18:30:11 +0200

    Fixed Imports/Cleaned Code

--- frontEnd/components/storeScreen.js
+++ frontEnd/components/storeScreen.js
@@ -1,19 +1,14 @@
 import 'react-native-gesture-handler';
 import React, { Component } from "react";
 import {
-  StyleSheet,
   Text,
   View,
-  SafeAreaView,
-  TouchableOpacity,
-  StatusBar,
   FlatList,
   Modal,
 } from "react-native";
 import { useNavigation } from '@react-navigation/native';
 import styles from '../componentStyles.js'
-import { NavigationContainer } from '@react-navigation/native';
-import {Card, TextInput, IconButton, Button } from 'react-native-paper';
+import {Card, IconButton, Button } from 'react-native-paper';
 import axios from 'axios';
 
 class StoreScreen extends Component {
@@ -30,7 +25,7 @@ class StoreScreen extends Component {
     specific_price: '',
     specific_description:'',
   }
-
+  //Card set ups
   clickedItem = (data) => {
     this.setState({show:true})
     this.setState({specific_item_name:data.item_name})
@@ -48,7 +43,7 @@ class StoreScreen extends Component {
     )
   
   }
-  
+  //Classroom Store Setups
   getClassCode(){
     axios.get(getIP()+'/students/class_code/')
     .then(response => {
@@ -127,31 +122,27 @@ class StoreScreen extends Component {
         </View>
 
         <Modal
-            transparent = {true}
-            visible = {this.state.show}
-            >
-              <View style = {{backgroundColor:'#000000aa', flex:1}}>
-                <View style = {{backgroundColor:'#ffffff', margin:50, padding:40, borderRadius:10, marginTop:200, marginBottom:400, flex:1}} >
-                  <View style = {{position: 'absolute', right:0, top:0}}>
-                    <IconButton
-                      icon="close-box-outline"
-                      color= 'grey'
-                      size={20}
-                      onPress={() => {this.setState({show:false})}}
-                    />
-                  </View>
-                  <Text style = {styles.subHeader}>{this.state.specific_item_name}</Text>
-                  <Text style = {{marginTop:10, marginBottom:10}}>{this.state.specific_description}</Text>
-                  <Button mode='contained' onPress={() => {this.setState({show:false})}}>Purchase for {this.state.specific_price}</Button>
-                </View>
-
+          transparent = {true}
+          visible = {this.state.show}
+        >
+          <View style = {{backgroundColor:'#000000aa', flex:1}}>
+            <View style = {{backgroundColor:'#ffffff', margin:50, padding:40, borderRadius:10, marginTop:200, marginBottom:400, flex:1}} >
+              <View style = {{position: 'absolute', right:0, top:0}}>
+                <IconButton
+                  icon="close-box-outline"
+                  color= 'grey'
+                  size={20}
+                  onPress={() => {this.setState({show:false})}}
+                />
               </View>
-
-
-            </Modal>
+              <Text style = {styles.subHeader}>{this.state.specific_item_name}</Text>
+              <Text style = {{marginTop:10, marginBottom:10}}>{this.state.specific_description}</Text>
+              <Button mode='contained' onPress={() => {this.setState({show:false})}}>Purchase for {this.state.specific_price}</Button>
+            </View>
+          </View>
+        </Modal>
 
       </View>
-      
     )
   }
 }
