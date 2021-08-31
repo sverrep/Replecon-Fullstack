c761af16c228cf3e16822a63cbb27b1cccf49262
sverrep <sverrep@users.noreply.github.com>
2021-08-31 16:04:51 +0200

    Added teacher store creation and item functionality

--- frontEnd/web/src/Apps/TeacherClass/TeacherClassApp.js
+++ frontEnd/web/src/Apps/TeacherClass/TeacherClassApp.js
@@ -12,6 +12,7 @@ import InputGroup from 'react-bootstrap/InputGroup'
 import FormControl from 'react-bootstrap/FormControl'
 import Modal from 'react-bootstrap/Modal'
 import FloatingLabel from 'react-bootstrap/FloatingLabel'
+import Card from 'react-bootstrap/Card'
 
 
 class TeacherClassApp extends React.Component {
@@ -32,8 +33,21 @@ class TeacherClassApp extends React.Component {
         payout_rate: "",
         student_savings: [],
         banks: [],
+        shops: [],
+        shop_id: 0,
+        classHasShop: false,
+        items: [],
+        showUpdateItem:false,
+        showAddItem:false,
+        showCreateStore: false,
+        item_name: '',
+        item_desc: '',
+        item_price: 0,
+        item_id: 0,
+        store_name: '',
         error: '',
         bank_error: '',
+        store_error: '',
         classHasBank: false,
         redirect_profile: false,
         bankModalShow: false,
@@ -52,6 +66,7 @@ class TeacherClassApp extends React.Component {
   componentDidMount(){
     this.getClassStudents()
     this.getBanks()
+    this.getShops()
   }
 
   studentClicked(student){
@@ -139,6 +154,17 @@ class TeacherClassApp extends React.Component {
       }
     }
 
+    renderStoreItems(item, i){
+      return(
+        <Card key={i}>
+          <Card.Body>
+            <Card.Title> {item.item_name} {item.price} </Card.Title>
+            <Card.Text> {item.description} <Button variant="primary" onClick={() => this.clickedItem(item.item_name, item.price, item.description, item.id)}>Update Item</Button> </Card.Text>
+          </Card.Body>
+        </Card>
+      )
+    }
+
     handleChange(e) {
       const field = e.target.id
       this.setState({ [field] : e.target.value });
@@ -184,6 +210,112 @@ class TeacherClassApp extends React.Component {
       this.setState({show:false})
     }
   }
+
+  getShops(){
+    axios.get(getIP()+'/shops/')
+    .then(response => {
+      this.setState({shops: response.data})
+      this.checkForShop(response.data)
+    })
+    .catch(error => console.log(error))
+  }
+
+  checkForShop(shops){
+    for(let i = 0; i <= Object.keys(shops).length - 1; i++){
+      if(shops[i].class_code === this.state.class_code){
+        this.setState({classHasShop:true, store_name: shops[i].shop_name, shop_id: shops[i].id})
+      }
+    }
+    this.getItems()
+  }
+
+  getItems(){
+    axios.get(getIP()+'/items/')
+    .then(response => {
+      this.getShopItems(response.data)
+    })
+    .catch(error => console.log(error))
+  }
+
+  getShopItems(allItems){
+    var ar = []
+    for(let i = 0; i<=Object.keys(allItems).length -1; i++)
+    {
+      if(allItems[i].shop_id === this.state.shop_id)
+      {
+        ar.push(allItems[i])
+      }
+    }
+    this.setState({items:ar})
+  }
+
+  createNewStore(){
+    if(this.state.store_name !== "")
+    {
+      axios.post(getIP()+'/shops/', {
+        shop_name: this.state.store_name,
+        class_code: this.state.class_code,
+      })
+      .then(response => {
+        this.setState({showCreateStore: false, classHasShop: true, shop_id: response.data.id})
+      })
+      .catch(error => console.log(error))
+    }
+    else
+    {
+      this.setState({store_error: "Please enter a valid store name"})
+    }
+  }
+
+  addItem(){
+    if(this.validateItem())
+    {
+      axios.post(getIP()+'/items/', {
+        item_name: this.state.item_name,
+        description: this.state.item_desc,
+        price: this.state.item_price,
+        shop_id: this.state.shop_id,
+      })
+      .then(response => {
+        this.setState({showAddItem: false, items: [...this.state.items, response.data]})
+      })
+      .catch(error => console.log(error))
+    }
+  }
+
+  updateItem(){
+    if(this.validateItem())
+    {
+      axios.put(getIP()+'/items/'+ this.state.item_id, {
+        item_name: this.state.item_name,
+        description: this.state.item_desc,
+        price: this.state.item_price,
+        shop_id: this.state.shop_id,
+      })
+      .then(response => {
+        this.setState({showUpdateItem: false, items: []})
+        this.getItems()
+      })
+      .catch(error => console.log(error))
+    }
+  }
+
+  clickedItem(name, price, description, id){
+    this.setState({showUpdateItem:true})
+    this.setState({item_name:name})
+    this.setState({item_price:price})
+    this.setState({item_desc:description})
+    this.setState({item_id:id})
+  }
+
+  deleteItem(){
+    axios.delete(getIP()+'/items/'+ this.state.item_id)
+    .then(response => {
+      this.setState({showUpdateItem: false, items: []})
+      this.getItems()
+    })
+    .catch(error => console.log(error))
+}
 
   getBanks(){
     axios.get(getIP()+'/banks/')
@@ -245,10 +377,10 @@ class TeacherClassApp extends React.Component {
   hasBank(){
     return(
       <Col>
-        <h4>Class Bank</h4>
+        <h4>{this.state.class_name} Bank</h4>
         <h6>Interest Rate: {this.state.interest_rate}</h6>
         <h6>Payout Rate: {this.state.payout_rate}</h6>
-        <ListGroup>
+        <ListGroup className="small-group">
           {this.state.student_savings.map((student,i) => this.renderStudentSavings(student, i))}
         </ListGroup>
         <Button className="pay-btns" onClick={() => this.setState({updateBankModalShow: true})}>Update Bank Rates</Button>
@@ -259,9 +391,13 @@ class TeacherClassApp extends React.Component {
 
   hasNoBank(){
     return (
-      <Button variant="primary" onClick={() => this.setState({bankModalShow: true})}>
-        Create A Bank
-      </Button>
+      <Col>
+        <h4>This class has no bank</h4>
+        <Button variant="primary" onClick={() => this.setState({bankModalShow: true})}>
+          Create A Bank
+        </Button>
+        {this.createBankModal()}
+      </Col>
     );
   }
 
@@ -279,6 +415,83 @@ class TeacherClassApp extends React.Component {
       )
     }
   }
+
+  hasShop(){
+    return (
+      <Col>
+        <h4>{this.state.store_name} Store</h4>
+        <ListGroup className="small-group">
+          {this.state.items.map((item,i) => this.renderStoreItems(item, i))}
+        </ListGroup>
+        <Button variant="primary" className="pay-btns" onClick={() => this.setState({showAddItem: true})}>
+          Add Item
+        </Button>
+        {this.addItemModal()}
+        {this.updateItemModal()}
+      </Col>
+    );
+  }
+
+  hasNoShop(){
+    return (
+      <Col>
+        <h4>This class has no store</h4>
+        <Button variant="primary" onClick={() => this.setState({showCreateStore: true})}>
+          Create A Store
+        </Button>
+        {this.createStoreModal()}
+      </Col>
+    );
+}
+
+  renderShopView(){
+    if(this.state.classHasShop)
+    {
+      return(
+        this.hasShop()
+      )
+    }
+
+    else{
+      return(
+        this.hasNoShop()
+      )
+    }
+  }
+
+  validateItem(){
+    if(this.state.item_name !== '')
+    {
+        if(!isNaN(this.state.item_price))
+        {
+            if(Math.sign(parseFloat(this.state.item_price)) === 1)
+            {
+                if(this.state.item_desc)
+                {
+                    return true
+                }
+                else
+                {
+                    this.setState({store_error: "Please enter a valid item description"})
+                }
+            }
+            else
+            {
+                this.setState({store_error: "Please enter a valid item price"})
+            }
+        }
+        else
+        {
+            this.setState({store_error: "Please enter a number"})
+        }
+        
+        
+    }
+    else
+    {
+        this.setState({store_error: "Please enter a valid item name"})
+    }
+}
 
   validateBank(){
     if(this.state.interest_rate !== '')
@@ -320,7 +533,6 @@ class TeacherClassApp extends React.Component {
   }
 
   createNewBank(){
-    console.log("yo")
     if(this.validateBank())
     {
       axios.post(getIP()+'/banks/', {
@@ -365,16 +577,16 @@ class TeacherClassApp extends React.Component {
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <FloatingLabel label="Bank Interest Rate" className="bank-input">
+          <FloatingLabel label="Bank Interest Rate" className="modal-input">
             <FormControl id='interest_rate' placeholder="Interest Rate" onChange={this.handleChange}/>
           </FloatingLabel>
-          <FloatingLabel label="Bank Payout Rate in Weeks" className="bank-input">
+          <FloatingLabel label="Bank Payout Rate in Weeks" className="modal-input">
             <FormControl id='payout_rate' placeholder="Payout Rate" onChange={this.handleChange} />
           </FloatingLabel>
           <p>{this.state.bank_error}</p>
         </Modal.Body>
         <Modal.Footer>
-          <Button className="bank-btn" onClick={() => this.createNewBank()}>Create Bank</Button>
+          <Button className="modal-btn" onClick={() => this.createNewBank()}>Create Bank</Button>
         </Modal.Footer>
       </Modal>
     );
@@ -394,16 +606,128 @@ class TeacherClassApp extends React.Component {
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <FloatingLabel label="Bank Interest Rate" className="bank-input">
+          <FloatingLabel label="Bank Interest Rate" className="modal-input">
             <FormControl id='interest_rate' placeholder="Interest Rate" defaultValue={this.state.interest_rate} onChange={this.handleChange}/>
           </FloatingLabel>
-          <FloatingLabel label="Bank Payout Rate in Weeks" className="bank-input">
+          <FloatingLabel label="Bank Payout Rate in Weeks" className="modal-input">
             <FormControl id='payout_rate' placeholder="Payout Rate" defaultValue={this.state.payout_rate} onChange={this.handleChange} />
           </FloatingLabel>
           <p>{this.state.bank_error}</p>
         </Modal.Body>
         <Modal.Footer>
-          <Button className="bank-btn" onClick={() => this.updateBankRates()}>Update Bank</Button>
+          <Button className="modal-btn" onClick={() => this.updateBankRates()}>Update Bank</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+
+  createStoreModal(){
+    return (
+      <Modal
+        show={this.state.showCreateStore}
+        onHide={() => this.setState({showCreateStore: false})}
+        size="lg"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>
+            Create A Store
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <FloatingLabel label="Store Name" className="modal-input">
+            <FormControl id='store_name' placeholder="Interest Rate" onChange={this.handleChange}/>
+          </FloatingLabel>
+          <p>{this.state.store_error}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button className="modal-btn" onClick={() => this.createNewStore()}>Create Bank</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+
+  addItemModal(){
+    return (
+      <Modal
+        show={this.state.showAddItem}
+        onHide={() => this.setState({showAddItem: false})}
+        size="lg"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>
+            Add An Item
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Container>
+            <Row>
+              <Col xs={12} md={8}>
+                <FloatingLabel label="Item Name" className="modal-input">
+                  <FormControl id='item_name' placeholder="Item Name" onChange={this.handleChange}/>
+                </FloatingLabel>
+              </Col>
+              <Col xs={6} md={4}>
+                <FloatingLabel label="Item Price" className="modal-input">
+                  <FormControl id='item_price' placeholder="Item Price" onChange={this.handleChange}/>
+                </FloatingLabel>
+              </Col>
+            </Row>
+            <Row>
+              <FloatingLabel label="Item Description" className="modal-input">
+                <FormControl id='item_desc' placeholder="Item Description" onChange={this.handleChange} />
+              </FloatingLabel>
+            </Row>
+          </Container>
+          <p>{this.state.store_error}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button className="modal-btn" onClick={() => this.addItem()}>Add Item</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+
+  updateItemModal()
+  {
+    return (
+      <Modal
+        show={this.state.showUpdateItem}
+        onHide={() => this.setState({showUpdateItem: false})}
+        size="lg"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>
+            Update An Item
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Container>
+            <Row>
+              <Col xs={12} md={8}>
+                <FloatingLabel label="Item Name" className="modal-input">
+                  <FormControl id='item_name' placeholder="Item Name" defaultValue={this.state.item_name} onChange={this.handleChange}/>
+                </FloatingLabel>
+              </Col>
+              <Col xs={6} md={4}>
+                <FloatingLabel label="Item Price" className="modal-input">
+                  <FormControl id='item_price' placeholder="Item Price" defaultValue={this.state.item_price} onChange={this.handleChange}/>
+                </FloatingLabel>
+              </Col>
+            </Row>
+            <Row>
+              <FloatingLabel label="Item Description" className="modal-input">
+                <FormControl id='item_desc' placeholder="Item Description" defaultValue={this.state.item_desc} onChange={this.handleChange} />
+              </FloatingLabel>
+            </Row>
+          </Container>
+          <p>{this.state.store_error}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button className="modal-btn" onClick={() => this.updateItem()}>Update Item</Button>
+          <Button className="modal-btn" onClick={() => this.deleteItem()}>Delete Item</Button>
         </Modal.Footer>
       </Modal>
     );
@@ -446,11 +770,10 @@ class TeacherClassApp extends React.Component {
                 <p>Class Taxes</p>
               </Row>
               <Row className="store-row">
-                <p>Class Store</p>
+                {this.renderShopView()}
               </Row>
               <Row className="bank-row">
                 {this.renderBankView()}
-                {this.createBankModal()}
               </Row>
             </Col>
           </Row>
