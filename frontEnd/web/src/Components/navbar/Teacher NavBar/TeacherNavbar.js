b8d398258666b6da837ab76b37defa8c4e90efc8
sverrep <sverrep@users.noreply.github.com>
2021-11-04 20:20:07 +0100

    Redesigned Taxes screen and added functionality to tax class

--- frontEnd/web/src/Components/navbar/Teacher NavBar/TeacherNavbar.js
+++ frontEnd/web/src/Components/navbar/Teacher NavBar/TeacherNavbar.js
@@ -13,8 +13,8 @@ export default function navbar(htext, current_class, teacher_id){
         <Container>
             <Navbar.Brand className="navbar-header">{htext}</Navbar.Brand>
             <Nav className="navbar-btns">
-                <Button variant="secondary" className="navbar-btn-current"><Link className="navbar-link-current" to={{pathname: `/Class/${current_class.class_code}/students`, state: {class: current_class, teacher_id: teacher_id}}}>Students</Link></Button>
-                <Button variant="light" className="navbar-btn"><Link className="navbar-link" to={{pathname: `/Class/${current_class.class_code}/features`, state: {class: current_class, teacher_id: teacher_id}}}>Taxes</Link></Button>
+                <Button variant="light" className="navbar-btn"><Link className="navbar-link" to={{pathname: `/Class/${current_class.class_code}/students`, state: {class: current_class, teacher_id: teacher_id}}}>Students</Link></Button>
+                <Button variant="light" className="navbar-btn"><Link className="navbar-link" to={{pathname: `/Class/${current_class.class_code}/taxes`, state: {class: current_class, teacher_id: teacher_id}}}>Taxes</Link></Button>
                 <Button variant="light" className="navbar-btn"><Link className="navbar-link" to={{pathname: `/Class/${current_class.class_code}/features`, state: {class: current_class, teacher_id: teacher_id}}}>Store</Link></Button>
                 <Button variant="light" className="navbar-btn"><Link className="navbar-link" to={{pathname: `/Class/${current_class.class_code}/features`, state: {class: current_class, teacher_id: teacher_id}}}>Bank</Link></Button>
                 <Button variant="secondary-outline" className="navbar-btn-class-list"><Link className="navbar-link" to={{pathname: `/Profile/`, state: {role: "Teacher"}}}>Class List</Link></Button>
