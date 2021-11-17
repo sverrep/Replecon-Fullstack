f9d3e315388e9c37eb20158d66d9190d97c17848
steveno06 <61500424+steveno06@users.noreply.github.com>
2021-11-17 17:58:09 +0100

    Student Store Tests

--- backEnd/replecon/replecon/settings.py
+++ backEnd/replecon/replecon/settings.py
@@ -197,4 +197,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'noreplyreplecon@gmail.com'
-EMAIL_HOST_PASSWORD = 'Replecon12!'
+EMAIL_HOST_PASSWORD = '8hR?Z9_XY^`bW53W'
