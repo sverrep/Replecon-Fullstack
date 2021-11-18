8127336d9b5f58c8be76034e2167694af48304f9
steveno06 <61500424+steveno06@users.noreply.github.com>
2021-11-18 14:14:52 +0100

    fix imports

--- frontEnd/web/src/Apps/StudentStore/StudentStoreApp.test.js
+++ frontEnd/web/src/Apps/StudentStore/StudentStoreApp.test.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import ReactDom from 'react-dom';
 import StudentStoreApp from './StudentStoreApp.js'
-import renderer from 'react-test-renderer'
 import axios from 'axios'
 
 jest.mock('axios');
